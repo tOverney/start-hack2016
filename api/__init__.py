9cee5bbce19d2212236fe239e4f0a8f7708aa057
tharvik <tharvik@users.noreply.github.com>
2016-03-12 14:19:00 +0100

    remove text_to_speech

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,8 @@
-from api.language_translation import LanguageTranslation
-from api.text_to_speech import TextToSpeech
 from api.text_insight import TextInsight
+from api.text_to_speech import TextToSpeech
 
 
 class Api:
     def __init__(self):
-        self.language_translation = LanguageTranslation()
         self.text_to_speech = TextToSpeech()
         self.text_insight = TextInsight()
