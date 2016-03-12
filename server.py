fec273f1aef878724436c864cbb807ab96b6869b
tharvik <tharvik@users.noreply.github.com>
2016-03-12 04:13:53 +0100

    add django

--- server.py
+++ server.py
@@ -1,21 +1,23 @@
 import os
+
+import sys
+
 try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
+    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+    from SocketServer import TCPServer as Server
 except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
 
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carousel.settings")
 
+from django.core.management import execute_from_command_line
+
+args = ['runserver', PORT]
+args.append(sys.argv)
+execute_from_command_line(args)
