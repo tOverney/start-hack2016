daa7ada124c7e8e22445bf8d266f862810212361
tharvik <tharvik@users.noreply.github.com>
2016-03-12 04:44:54 +0100

    manage.py add port if needed

--- manage.py
+++ manage.py
@@ -5,6 +5,10 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carousel.settings")
 
+    port = os.getenv('PORT', '8000')
+
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    args = sys.argv[:]
+    args.append(port)
+    execute_from_command_line(args)
