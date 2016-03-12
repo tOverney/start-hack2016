bf35d84b1ac2670d0f2316a5768757dba9cb45a6
tharvik <tharvik@users.noreply.github.com>
2016-03-12 04:54:51 +0100

    revert manage.py

--- manage.py
+++ manage.py
@@ -5,10 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carousel.settings")
 
-    port = os.getenv('PORT', '8000')
-
     from django.core.management import execute_from_command_line
 
-    args = sys.argv[:]
-    args.append(port)
-    execute_from_command_line(args)
+    execute_from_command_line(sys.args)
